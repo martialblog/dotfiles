23087aaeb29e5a42076dc28b265894f3a3d3c175
Markus <markus@martialblog.de>
2016-02-24 18:59:44 +0100

    Enabled whitespace-mode in emacs... Is is emacs or Emacs? I don't know.

--- emacs.d/init-local.el
+++ emacs.d/init-local.el
@@ -1,3 +1,13 @@
+;; My local emacs stuff
+
+;; Using https://github.com/purcell/emacs.d
+;; git clone https://github.com/purcell/emacs.d.git ~/.emacs.d 
+;; Maybe use M-x package-refresh-contents 
+
+;; Whitespace mode to catch those tricky characters
+(global-whitespace-mode 1)
+
+;; Neotree
 (add-to-list 'load-path "~/.emacs.d/elpa/neotree-20160214.532/")
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
