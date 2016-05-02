985356cf5eacf6c1f55c253d7ffe99b9b8d07fa2
Markus Opolka <markus@martialblog.de>
2016-05-02 08:54:28 +0200

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Since I don't wanna use Puppet I'm gonna be oldschool and Shell Script it.
+
+# apt-get install emacs24 i3 i3blocks virtualbox vagrant python3-pip zsh
+# unity-greeter-badges
+
+# pip3 install nltk jedi rope py3k importmagic autopep8 yapf flake8
+# elpy for emacs
+
+# Cloudstation.deb
+
+# git clone stuff (purcell, config)
+# oh my zsh
+# syslinks
