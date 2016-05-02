3669a109f6a82860c0f19fcbb88144ebaa564c89
Markus Opolka <markus@martialblog.de>
2016-05-02 13:03:56 +0200

    Added Cloudstation deb download

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # Since I don't wanna use Puppet I'm gonna be oldschool and script it.
 
 # apt installation
@@ -8,14 +7,14 @@ sudo apt-get install emacs i3 i3blocks virtualbox vagrant python3-pip zsh unity-
 # Python Stuff
 pip3 --user install nltk jedi rope py3k importmagic autopep8 yapf flake8
 
-#TODO
-# Install elpy for emacs
-# Get and install cloudstation.deb
-# git clone stuff (purcell, config)
-
+# github
 mkdir ~/github
-
 git clone https://github.com/purcell/emacs.d ~/.emacs.d
 git clone https://github.com/martialblog/config ~/github/config
 
+#TODO
+# Get cloudstation.deb
+wget -P /home/markus/Downloads/ https://global.download.synology.com/download/Tools/CloudStationDrive/3.2-3501/Ubuntu/Installer/x86_64/synology-cloud-station-3501.x86_64.deb
+
 # Syslinks
+# Install elpy for emacs
