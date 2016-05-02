93d63ffe3bcd7e5454910993c71e088aaaa6e5f0
Markus Opolka <markus@martialblog.de>
2016-05-02 12:59:07 +0200

    Fixed todo list at loginshell

--- zsh.d/todo/todo.plugin.zsh
+++ zsh.d/todo/todo.plugin.zsh
@@ -4,6 +4,6 @@ TODO_FILE="/home/markus/CloudStation/Notes/notes.org"
 
 if [ ! -a $TODO_FILE ]; then
     echo -e "        \033[0;34mTasks\033[0m"
-    grep --color "** TODO" $TODO_FILE
+    grep --color "^** TODO" $TODO_FILE
     echo ""
 fi
