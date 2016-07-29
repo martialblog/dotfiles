888722973c62959d9ca95e1ed9c2b41194e40d06
Markus Opolka <markus.opolka@iis.fraunhofer.de>
2016-07-29 09:19:19 +0200

    Remove dead code

--- emacs.d/init-local.el
+++ emacs.d/init-local.el
@@ -1,5 +1,3 @@
-;;; My local emacs stuff
-
 ;; Using https://github.com/purcell/emacs.d
 ;; git clone https://github.com/purcell/emacs.d.git ~/.emacs.d 
 ;; Maybe use M-x package-refresh-contents 
@@ -16,11 +14,6 @@
 
 ;; xclip Mode for copy-paste
 ;; (xclip-mode 1)
-
-;; Neotree
-;;(add-to-list 'load-path "~/.emacs.d/elpa/neotree-20160214.532/")
-;;(require 'neotree)
-;;(global-set-key [f8] 'neotree-toggle)
 
 ;;PYTHON Stuff
 (eval-after-load 'python
