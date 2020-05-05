edc88576e904020aae704112a3df1ae15bc6d91f
Markus Opolka <markus@martialblog.de>
2020-05-05 11:00:00 +0200

    Yabar with dynamic color for battery

--- yabar.d/battery.sh
+++ yabar.d/battery.sh
@@ -2,7 +2,8 @@
 
 LEVEL=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '$0~/percentage/{print $2}' |  egrep -o '[0-9]+')
 
-if [ $LEVEL -le 15 ]
-then
-   notify-send --icon=gtk-info "Battery Critical" "${LEVEL}"
+if [ $LEVEL -le 15 ]; then
+    echo "!Y BG 0xFFFF0000 Y! BAT ${LEVEL}%"
+else
+    echo "!Y BG 0xFF00AA00 Y! BAT ${LEVEL}%"
 fi
